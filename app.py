7f5caf9a7ac06927da793a62f21f95d4c490c691
lazaro <lazlemlop@gmail.com>
2021-04-25 20:03:46 -0400

    initial commit

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 
 
-
+url='postgresql://quwmbantdzhpao:877234c1f72fd64bc78717fbc0f1d833e5c1e2c7e98af612e67badef983ad03a@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d4mlir0vcgc3ho'
 
 
 app = Flask(__name__)
 
 scheduler = BackgroundScheduler()
-scheduler.add_jobstore('sqlalchemy', url='sqlite:///new.db')
+scheduler.add_jobstore('sqlalchemy', url=url)
 
 
 login_manager = LoginManager()
--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     SECRET_KEY = 'Bj97zUiLgVPDR07RMXYP'
     ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL1')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://quwmbantdzhpao:877234c1f72fd64bc78717fbc0f1d833e5c1e2c7e98af612e67badef983ad03a@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d4mlir0vcgc3ho'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT = 465
