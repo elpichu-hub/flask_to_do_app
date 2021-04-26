c6924e1f1a6ebd50777cc1b2da0f2111bd36d638
lazaro <lazlemlop@gmail.com>
2021-04-26 12:30:55 -0400

    initial commit

--- routes.py
+++ routes.py
@@ -34,7 +34,7 @@ def printing():
 
 
 
-check_hour_job = scheduler.add_job(task_date_check, 'interval', seconds=60, id='myjob', replace_existing=True)  
+check_hour_job = scheduler.add_job(task_date_check, 'interval', seconds=50, id='myjob', replace_existing=True)  
 scheduler.start()
 
 
