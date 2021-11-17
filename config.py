8f8318bd78c3258a9f9d52af088948d249890e77
elpichu-hub <80866847+elpichu-hub@users.noreply.github.com>
2021-11-17 14:50:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -14,21 +14,21 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'etubrute56@gmail.com'
-    MAIL_PASSWORD = 'compay2nd'
+    MAIL_USERNAME = 'email'
+    MAIL_PASSWORD = 'password'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     
 
 class ProductionConfig(Config):
-    SECRET_KEY = 'Bj97zUiLgVPDR07RMXYP'
+    SECRET_KEY = 'secret key'
     ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://quwmbantdzhpao:877234c1f72fd64bc78717fbc0f1d833e5c1e2c7e98af612e67badef983ad03a@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d4mlir0vcgc3ho'
+    SQLALCHEMY_DATABASE_URI = 'data base url'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'etubrute56@gmail.com'
-    MAIL_PASSWORD = 'compay2nd'
+    MAIL_USERNAME = 'usename@gmail.com'
+    MAIL_PASSWORD = 'password'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
